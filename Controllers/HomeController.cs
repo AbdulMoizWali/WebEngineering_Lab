c766d684a7dcf6a0873eefe304c664d006b0e952
AbdulMoiz <85098925+AbdulMoizWali@users.noreply.github.com>
2023-11-02 20:44:54 +0500

    feat: Data Annotation Task Completed
    Validation in Data Model and created View for that

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebEngineering.Models;
+
+namespace WebEngineering.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Employee> employees_data = new List<Employee>();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            employees();
+            return View(employees_data);
+        }
+
+        public ActionResult Create(Employee employee)
+        {
+            return View(employee);
+        }
+
+        [NonAction]
+        public List<Employee> employees()
+        {
+            employees_data.Add(
+                new Employee(
+                    1,
+                    "Abdul Moiz1", 
+                    "sp21bsse0005@maju.edu.pk",
+                    21,
+                    "0311xxxxxxx", 
+                    new DateTime(2021, 9, 1), 
+                    "Pakistan"
+                )
+             );
+            employees_data.Add(
+                new Employee(
+                    2,
+                    "Abdul Moiz2",
+                    "sp21bsse0005@maju.edu.pk",
+                    21,
+                    "0311xxxxxxx",
+                    new DateTime(2021, 9, 1),
+                    "Pakistan"
+                )
+             );
+            employees_data.Add(
+                new Employee(
+                    3,
+                    "Abdul Moiz3",
+                    "sp21bsse0005@maju.edu.pk",
+                    21,
+                    "0311xxxxxxx",
+                    new DateTime(2021, 9, 1),
+                    "Pakistan"
+                )
+             );
+
+            return employees_data;
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebEngineering.Models
+{
+    public class Employee
+    {
+        public Employee() { }
+        public Employee(int id, string name, string email, int age, string phoneContact, DateTime dOJ, string country)
+        {
+            Id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+            this.phoneContact = phoneContact;
+            DOJ = dOJ;
+            this.country = country;
+        }
+
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name of employee is required")]
+        [DisplayName("Name")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage ="Email of employee is required")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$", ErrorMessage ="Must be valid email")]
+        [DisplayName("Email")]
+        public string email { get; set; }
+
+
+        [Range(16,60)]
+        [DisplayName("Age")]
+        public int age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [DisplayName("Phone Contact")]
+        public string phoneContact { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date of Joining")]
+        public DateTime DOJ { get; set; }
+
+        [DisplayName("Country")]
+        public string country { get; set; }
+    }
+}
